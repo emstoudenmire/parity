9e0c7b317fcd9e267439a73a3667d8d8848ff970
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-04-12 11:54:52 -0400

    Added a loop and summary around multiple sampleMPS experiments

--- learn.cc
+++ learn.cc
@@ -1,8 +1,17 @@
+#include <algorithm>
 #include "bitstring.h"
 using namespace itensor;
 using std::vector;
 using std::string;
 
+template<typename Container>
+void
+randshuffle(Container & C)
+    {
+    static std::random_device rd;
+    static std::mt19937 g(rd());
+    std::shuffle(C.begin(), C.end(), g);
+    }
 
 MPS
 makeMPS(SiteSet const& sites,
@@ -81,15 +90,18 @@ makeMPS(SiteSet const& sites,
     return psi;
     }
 
+template<typename SPolicy>
 MPS
 sampleMPS(SiteSet const& sites,
+          SPolicy & sp,
           Args const& args = Args::global())
     {
     auto N = length(sites);
     auto rthreshold = args.getReal("RhoThreshold");
     auto maxdim = args.getInt("MaxDim");
     auto minsamples = args.getInt("MinSamples");
-    auto nstep = 64;
+    auto nstep = args.getInt("SampleStep");
+    auto pause_step = args.getBool("PauseStep",false);
 
     auto phi = [&sites](BitString B, int n) -> ITensor
         {
@@ -103,70 +115,118 @@ sampleMPS(SiteSet const& sites,
 
     Index eind;
 
+    struct Sample
+        {
+        BitString b;
+        ITensor env;
+        ITensor phi;
+        };
+    auto computeSample = [&phi]
+                         (MPS const& psi,
+                          SPolicy & sp,
+                          int n) -> Sample
+        {
+        Sample s;
+        s.b = sp.next();
+        s.env = ITensor(1);
+        for(int k = 1; k < n; ++k)
+            {
+            s.env = psi(k)*s.env*phi(s.b,k);
+            }
+        s.phi = phi(s.b,n);
+        return s;
+        };
+
     for(int n : range1(1,N))
         {
         printfln("n = %d",n);
+        sp.reset();
+
         ITensor rho,rho_prev;
         bool converged = false;
 
-        auto samples = stdx::reserve_vector<BitString>(minsamples);
+        auto samples = stdx::reserve_vector<Sample>(minsamples);
         for(int s = 1; s <= minsamples-nstep; ++s)
             {
-            samples.push_back(randomEven(N));
+            if(sp.done()) break;
+            samples.push_back(computeSample(psi,sp,n));
             }
 
+        auto trial_count = 1;
         while(not converged)
             {
+            int ninclude = 0;
             rho = ITensor();
             for(int i1 = 0; i1 < samples.size(); ++i1)
             for(int i2 = i1; i2 < samples.size(); ++i2)
                 {
-                auto b1 = samples[i1];
-                auto b2 = samples[i2];
-                bool include = compFrom(n+1,b1,b2);
+                auto s1 = samples[i1];
+                auto s2 = samples[i2];
+                bool include = compFrom(n+1,s1.b,s2.b);
                 if(include)
                     {
-                    //Make environment tensors
-                    auto env1 = ITensor(1);
-                    auto env2 = ITensor(1);
-                    for(int k = 1; k < n; ++k)
-                        {
-                        env1 = psi(k)*env1*phi(b1,k);
-                        env2 = psi(k)*env2*phi(b2,k);
-                        }
-
-                    auto wf1 = env1*phi(b1,n);
-                    auto wf2 = env2*phi(b2,n);
+                    auto wf1 = s1.env*s1.phi;
+                    auto wf2 = s2.env*s2.phi;
                     rho += wf1*prime(wf2);
                     if(i1 != i2) rho += wf2*prime(wf1);
+
+                    ++ninclude;
                     }
                 }
 
-            auto Tr = rho * delta(sites(n),prime(sites(n)));
-            if(eind) Tr *= delta(eind,prime(eind));
-            rho /= elt(Tr);
-
-            if(rho_prev)
+            if(rho)
                 {
-                converged = (norm(rho-rho_prev) < rthreshold);
-                }
-            rho_prev = rho;
+                auto TrT = rho * delta(sites(n),prime(sites(n)));
+                if(eind) TrT *= delta(eind,prime(eind));
+                auto Tr = elt(TrT);
+                if(Tr > 0.) rho /= Tr;
 
-            for(int s = 1; s <= nstep; ++s) samples.push_back(randomEven(N));
+                Real diff = 1.0;
+                if(rho_prev)
+                    {
+                    diff = norm(rho-rho_prev);
+                    converged = (diff < rthreshold);// && (norm(rho) > 0.);
+                    }
+                rho_prev = rho;
+
+                //
+                // Diagonalize rho just to visually inspect
+                //
+                auto [U,D,l] = diagHermitian(rho,{"Tags=","Link,"+format("n=%d",n)});
+                //auto eTr = 0.;
+                println("D = ");
+                for(int d = 1; d <= dim(l); ++d)
+                    {
+                    printfln("%d  %.5f",d,elt(D,d,d));
+                    //eTr += elt(D,d,d);
+                    }
+                //Print(eTr);
+                //
+                //
+                //
+
+                printfln("n=%d Trial %d: nsamples=%d,ninclude=%d, diff=%.5f",
+                         n,trial_count,samples.size(),ninclude,diff);
+
+                trial_count += 1;
+                }
+
+            //Expand the set of samples
+            for(int s = 1; s <= nstep; ++s) 
+                {
+                if(sp.done()) break;
+                samples.push_back(computeSample(psi,sp,n));
+                }
             }
 
-        printfln("rho_%d converged after %d samples",n,samples.size());
 
         auto Tr = rho * delta(sites(n),prime(sites(n)));
         if(eind) Tr *= delta(eind,prime(eind));
-        Print(Tr);
         rho /= elt(Tr);
-        PrintData(rho);
 
         auto [U,D,l] = diagHermitian(rho,{"Tags=","Link,"+format("n=%d",n),"MaxDim=",maxdim});
         //auto [U,D,l] = diagHermitian(rho,{"Tags=","Link,"+format("n=%d",n)});
         PrintData(U);
-        Print(norm(rho-U*D*prime(U)));
         eind = l;
         auto eTr = 0.;
         println("D = ");
@@ -176,7 +236,11 @@ sampleMPS(SiteSet const& sites,
             eTr += elt(D,d,d);
             }
         Print(eTr);
-        //PAUSE;
+
+        Real total = pow(2.,N-1);
+        Real pct = samples.size()/total*100.;
+        printfln("rho_%d converged after %d samples (%.3f%% of total)",n,samples.size(),pct);
+        if(pause_step) PAUSE;
 
         psi.set(n,U);
         }
@@ -256,17 +320,82 @@ exactMPS(SiteSet const& sites)
 Real
 bhattDist(MPS const& psi, SiteSet const& sites)
     {
-    return overlap(psi,exactMPS(sites));
+    return fabs(overlap(psi,exactMPS(sites)));
     }
 
-
-int
-main()
+struct PushSample
     {
-    int N = 6;
-    int maxDim = 3;
+    using storage = vector<BitString>;
+    storage data;
+    int n = 0;
 
-    //auto data = allEvenStrings(N);
+    PushSample(storage const& d) : data(d) { }
+
+    BitString
+    next() 
+        {
+        auto lastn = n;
+        n += 1;
+        return data.at(lastn);
+        }
+
+    void
+    reset() 
+        {
+        randshuffle(data);
+        n = 0;
+        }
+
+    bool
+    done() const
+        {
+        return (n >= data.size());
+        }
+    };
+
+struct GenSample
+    {
+    int n = 0;
+    int N = 0;
+
+    GenSample(int N_) : N(N_) { }
+
+    BitString
+    next() 
+        {
+        n += 1;
+        return randomEven(N);
+        }
+
+    void
+    reset() 
+        {
+        n = 0;
+        }
+
+    bool 
+    done() const { return false; }
+    };
+
+int 
+main(int argc, char* argv[])
+    {
+    if(argc != 2) return printfln("Usage: %s inputfile",argv[0]),0;
+
+    auto in = InputGroup(argv[1],"input");
+
+    auto N = in.getInt("N");
+    auto maxDim = in.getInt("maxDim");
+    //auto ninclude = in.getInt("ninclude",0);
+    auto rho_threshold = in.getReal("rho_threshold",0.1);
+    auto minsamples = in.getInt("minsamples",0);
+    auto samplestep = in.getInt("samplestep",1);
+    auto pause_step = in.getYesNo("pause_step",false);
+    auto nrepeat = in.getInt("nrepeat",1);
+
+    auto data = allEvenStrings(N);
+    printfln("Data set size = %d",data.size());
+    //writeToFile(format("data%d",N),data);
 
     //int Nsample = 1000;
     //auto data = vector<BitString>(Nsample);
@@ -275,12 +404,39 @@ main()
 
     auto sites = SiteSet(N,2);
 
-    //auto psi = makeMPS(sites,data,{"MaxDim=",maxDim});
-    auto psi = sampleMPS(sites,{"RhoThreshold=",1E-2,
-                                "MinSamples=",500,
-                                "MaxDim=",maxDim});
+    auto samplePolicy = PushSample(data);
 
-    Print(bhattDist(psi,sites));
+    //auto psi = makeMPS(sites,data,{"MaxDim=",maxDim});
+    vector<Real> distances;
+    for(int step = 1; step <= nrepeat; ++step)
+        {
+        auto psi = sampleMPS(sites,
+                             samplePolicy,
+                             {"RhoThreshold=",rho_threshold,
+                              "MinSamples=",minsamples,
+                              "SampleStep=",samplestep,
+                              "MaxDim=",maxDim,
+                              "PauseStep=",pause_step});
+        auto dist = bhattDist(psi,sites);
+        Print(dist);
+        if(not isnan(dist)) distances.push_back(dist);
+        println("Distances so far = ");
+        Real avg = 0;
+        Real avg2 = 0;
+        Real Nt = distances.size();
+        for(auto [n,d] : enumerate(distances))
+            {
+            printfln("%.12f",d);
+            avg += d;
+            avg2 += d*d;
+            }
+        avg /= Nt;
+        avg2 /= Nt;
+        println();
+        printfln("avg = %.12f",avg);
+        printfln("std dev = %.12f",sqrt(avg2-avg*avg));
+        sleep(3);
+        }
 
     //for(auto n : range1(20))
     //    {
